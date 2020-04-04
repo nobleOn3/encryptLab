2fdf47fb3b32136c48ba2af20e04ef4eb21dbc78
gclou <clo15008@byui.edu>
2020-04-03 21:39:54 -0600

    decrypt method

--- cipher02.h
+++ cipher02.h
@@ -83,14 +83,26 @@ public:
       // The decrypt pseudocode
       str += "decrypt(cipherText, password)\n";
       str += "   offset = offsetFromPassword(password)\n";
-      str += "   FOR p is all values in plainText\n";
-      str += "      row[r] += cipherText[p]\n";
-      str += "      if r equals offset\n";
-      str += "         r-- and increment is now false\n";
+      str += "   FOR p is zero while p is less than plainText.size()\n";
+      str += "      row[r] += plainText[p]\n";
+      str += "      if r equals key-1\n";
+      str += "         decrement r\n";
+      str += "         increment false\n";
       str += "      else if increment is true\n";
-      str += "         r++;\n";
-      str += "      else\n";
-      str += "         r--;\n";
+      str += "         increment r\n";
+      str += "      else if r equals zero\n";
+      str += "         increment r\n";
+      str += "         increment equals true\n";
+      str += "      else decrement r\n";
+      str += "   FOR r is zero while r is less than key\n";
+      str += "      FOR c is zero while row[r][c] is not NULL\n";
+      str += "         row[r][c] = plainText[p]\n";
+      str += "         increment p\n";
+      str += "   FOR rr = 0 while row[rr][cc] is not NULL\n";
+      str += "      if rr equals key-1 then decrement rr, increment cc, set increment = false\n";
+      str += "      else if rr is zero and increment is false then increment rr, increment cc, set increment equal to true\n";
+      str += "      else if increment is true then increment cc\n";
+      str += "      else decrement cc\n";
 
       return str;
    }
@@ -171,43 +183,38 @@ public:
    virtual std::string encrypt(const std::string & plainText,
                                const std::string & password)
    {
-      std::string cipherText;
+      std::string cipherText = "";
       // TODO - Add your code here
-      std::string editedText = removeSpace(plainText);
 
-      const int key = offsetFromPassword(password);
+      const int key = std::stoi(password);
       bool increment = true;
-      std::string row[key];
-      int count = 0;
-
+      std::string* row = new string[key];
       int r = 0;
-      for(int p = 0; p < editedText.size(); p++)
+      for (int p = 0; p < plainText.size(); p++)
       {
-         row[r] += editedText[p];
-         count++;
-         if(r == (key-1))
-         {
-            r--;
-            increment = false;
-         }
+          row[r] += plainText[p];
 
-         else if(increment == true)
-            r++;
-         else if(r == 0)
-         {
-            r++;
-            increment = true;
-         }
-         else
-         {
-            r--;
-         }
-         
+          if (r == (key - 1))
+          {
+              r--;
+              increment = false;
+          }
+          else if (increment)
+              r++;
+          else if (r == 0)
+          {
+              r++;
+              increment = true;
+          }
+          else
+          {
+              r--;
+          }
       }
 
-      for(int k = 0; k < key; k++)
+      for (int k = 0; k < key; k++)
       {
-         cipherText += row[k];
+          cipherText += row[k];
       }
 
       return cipherText;
@@ -220,10 +227,97 @@ public:
    virtual std::string decrypt(const std::string & cipherText,
                                const std::string & password)
    {
-      std::string plainText = cipherText;
-      // TODO - Add your code here
-      return plainText;
-   }
+       std::string plainText = cipherText;
+       // TODO - Add your code here
+
+       string end = "";
+       const int key = std::stoi(password);
+       bool increment = true;
+       std::string* row = new string[key];
+       int r = 0;
+       // FOR p is zero while p is less than plainText.size()
+       for (int p = 0; p < plainText.size(); p++)
+       {
+           // row[r] += plainText[p]
+           row[r] += plainText[p];
+
+           // if r equals key-1
+           if (r == (key - 1))
+           {
+               // decrement r
+               r--;
+               // increment false
+               increment = false;
+           }
+           // else if increment is true
+           else if (increment)
+           {
+               // increment r
+               r++;
+           }
+           // if r equals zero
+           else if (r == 0)
+           {
+               // increment r
+               r++;
+               // increment equals true
+               increment = true;
+           }
+           // else decrement r
+           else
+           {
+               r--;
+           }
+       }
+
+       int p = 0;
+       // FOR r is zero while r is less than key
+       for (int r = 0; r < key; r++)
+       {
+           // FOR c is zero while row[r][c] is not NULL
+           for (int c = 0; row[r][c] != NULL; c++)
+           {
+               // row[r][c] = plainText[p]
+               row[r][c] = plainText[p];
+               // increment p
+               p++;
+           }
+       }
+
+       int cc = 0;
+       increment = true;
+       // FOR rr is zero while row[rr][cc] is not NULL
+       for (int rr = 0; row[rr][cc] != NULL;)
+       {
+           end += row[rr][cc];
+           // if rr equals key-1 then decrement rr, increment cc, set increment = false
+           if (rr == (key - 1))
+           {
+               rr--;
+               if (key > 2)
+                   cc++;
+               increment = false;
+           }
+           // if rr is zero and increment is false then increment rr, increment cc, set increment equal to true
+           else if (rr == 0 && !increment)
+           {
+               rr++;
+               cc++;
+               increment = true;
+           }
+           // else if increment is true then increment cc
+           else if (increment)
+           {
+               rr++;
+           }
+           // else decrement cc
+           else
+           {
+               rr--;
+           }
+       }
+
+    return end;
 };
 
 #endif // CIPHER02_H
